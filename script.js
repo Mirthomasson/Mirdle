13c711f1154edea53e9f304e8c025567cb7a7498
MirThomasson <mirthomasson@gmail.com>
2022-10-18 13:24:58 -0800

    word-of-the-day

--- script.js
+++ script.js
@@ -4,23 +4,51 @@ const ANSWER_LENGTH = 5;
 
 
 async function init() {
-
     let currentGuess = '';
+    let currentRow = 0;
+
+    const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const resObj = await res.json();
+    const word = resObj.word.toUpperCase();
+    setLoading(false);
+
+    console.log(word);
 
     function addLetter (letter) {
         if (currentGuess.length < ANSWER_LENGTH) {
+            // add letter to the end
             currentGuess += letter;
         } else {
+            // replace the last letter
             currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
         }
 
-        letters[currentGuess.length -1].innerText = letter;
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length -1].innerText = letter;
+    }
+
+    async function commit() {
+        if (currentGuess.length != ANSWER_LENGTH) {
+            // do nothing
+            return;
+        }
+
+        // TODO validate the word
+
+        // TODO do all marking as "corect" "close" or "wrong"
+
+        // TODO did they win or lose?
+
+        currentRow++;
+        currentGuess = '';
+    }
+
+    function backspace() {
+        currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = '';
     }
 
     document.addEventListener('keydown', function handleKeyPress (event) {
         const action = event.key;
-
-        console.log(action);
 
         if (action === 'Enter') {
             commit();
@@ -36,6 +64,10 @@ async function init() {
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
+  }
+
+function setLoading(isLoading) {
+    loadingDiv.classList.toggle('hidden', !isLoading);
   }
 
 init();
